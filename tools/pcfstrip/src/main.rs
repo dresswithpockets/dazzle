ab748b606c491f936e3893ed91307edccfb1abb1
dresswithpockets <dresswithpockets@pm.me>
2026-01-20 02:25:26 -0700

    wip pcfstrip tool

---
+++ tools/pcfstrip/src/main.rs
@@ -0,0 +1,152 @@
+#![feature(file_buffered)]
+#![feature(seek_stream_len)]
+
+mod patch;
+
+use std::{collections::HashMap, env, fs::{File, OpenOptions}, io::{BufWriter, Seek}};
+
+use byteorder::WriteBytesExt;
+use bytes::{Buf, BufMut, BytesMut};
+use dmx::{Dmx, attribute::{Color, Vector3}};
+use pcf::{Attribute, Pcf};
+
+use crate::patch::PatchVpkExt;
+
+const DEFAULT_PCF_DATA: &[u8] = include_bytes!("default_values.pcf");
+
+fn main() -> anyhow::Result<()> {
+    let input_path = env::args().nth(1).expect("input pcf file path not given");
+    let output_path = env::args().nth(2).expect("output pcf file path not given");
+    let expected_size: u64 = env::args().nth(3).expect("expected size not given").parse()?;
+    
+    let default_system_map = get_particle_system_defaults();
+    // let default_attribute_map = get_default_attribute_map()?;
+    let default_attribute_map: HashMap<&str, Attribute> = HashMap::from([
+        ("operator start fadein", 0.0.into()),
+        ("operator end fadein", 0.0.into()),
+        ("operator start fadeout", 0.0.into()),
+        ("operator end fadeout", 0.0.into()),
+        ("Visibility Proxy Input Control Point Number", (-1).into()),
+        ("Visibility Proxy Radius", 1.0.into()),
+        ("Visibility input minimum", 0.0.into()),
+        ("Visibility input maximum", 1.0.into()),
+        ("Visibility Alpha Scale minimum", 0.0.into()),
+        ("Visibility Alpha Scale maximum", 1.0.into()),
+        ("Visibility Radius Scale minimum", 1.0.into()),
+        ("Visibility Radius Scale maximum", 1.0.into()),
+        ("Visibility Camera Depth Bias", 0.0.into())
+    ]);
+
+    let mut file = File::open_buffered(input_path)?;
+    let dmx = dmx::decode(&mut file)?;
+    let pcf: Pcf = dmx.try_into()?;
+    
+    // whats goin on with the 431st particle system? (0-indexed)
+    let pcf = pcf.defaults_stripped_nth(611, &default_system_map, &default_attribute_map);
+
+    let mut pcfs = pcf.into_connected();
+    let mut pcf = pcfs.pop().unwrap();
+    for from in pcfs {
+        pcf = pcf.merged(from).unwrap();
+    }
+
+    let dmx: Dmx = pcf.into();
+    let buf = BytesMut::new();
+    let mut writer = buf.writer();
+    dmx.encode(&mut writer)?;
+
+    let mut reader = writer.into_inner().reader();
+
+    let vpk_path = "/home/snale/.local/share/Steam/steamapps/common/Team Fortress 2/tf/tf2_misc_dir.vpk";
+    let mut vpk = vpk::from_path(vpk_path)?;
+    vpk.patch_file(&output_path, reader.get_ref().len() as u64, &mut reader)?;
+
+    // for _ in file.stream_len()?..expected_size {
+    //     file.write_u8(0)?;
+    // }
+
+    Ok(())
+}
+
+/// Decodes [`DEFAULT_PCF_DATA`] and produces a map of `functionName`, to a default attribute value map.
+fn get_default_attribute_map() -> anyhow::Result<HashMap<String, HashMap<String, pcf::Attribute>>> {
+    let mut reader = DEFAULT_PCF_DATA.reader();
+    let dmx = dmx::decode(&mut reader)?;
+    let pcf: Pcf = dmx.try_into()?;
+
+    let (_, symbols, root) = pcf.into_parts();
+    let (_, _, particle_systems, _) = root.into_parts();
+
+    let all_operators = particle_systems
+        .into_iter()
+        .flat_map(|system| {
+            [
+                system.constraints,
+                system.emitters,
+                system.forces,
+                system.initializers,
+                system.operators,
+                system.renderers,
+            ]
+        })
+        .flatten();
+
+    let mut operator_map = HashMap::new();
+    for operator in all_operators {
+        let value_map: HashMap<_, _> = operator
+            .attributes
+            .into_iter()
+            .filter(|(name_idx, _)| {
+                symbols.base.get_index_of("distance_bias").is_none_or(|idx| *name_idx as usize != idx)
+            })
+            .map(|(name_idx, attribute)| {
+                let name = symbols
+                    .base
+                    .get_index(name_idx as usize)
+                    .expect("this should never happen");
+                (name.clone(), attribute)
+            })
+            .collect();
+
+        operator_map.insert(operator.function_name, value_map);
+    }
+
+    Ok(operator_map)
+}
+
+fn get_particle_system_defaults() -> HashMap<&'static str, pcf::Attribute> {
+    HashMap::from([
+        ("batch particle systems", false.into()),
+        (
+            "bounding_box_min",
+            Vector3((-10.0).into(), (-10.0).into(), (-10.0).into()).into(),
+        ),
+        (
+            "bounding_box_max",
+            Vector3(10.0.into(), 10.0.into(), 10.0.into()).into(),
+        ),
+        ("color", Color(255, 255, 255, 255).into()),
+        ("control point to disable rendering if it is the camera", (-1).into()),
+        ("cull_control_point", 0.into()),
+        ("cull_cost", 1.0.into()),
+        ("cull_radius", 0.0.into()),
+        ("cull_replacement_definition", String::new().into()),
+        ("group id", 0.into()),
+        ("initial_particles", 0i32.into()),
+        ("material", "vgui/white".to_string().into()),
+        ("max_particles", 1000i32.into()),
+        ("maximum draw distance", 100_000.0.into()),
+        ("maximum sim tick rate", 0.0.into()),
+        ("maximum time step", 0.1.into()),
+        ("minimum rendered frames", 0.into()),
+        ("minimum sim tick rate", 0.0.into()),
+        ("radius", 5.0.into()),
+        ("rotation", 0.0.into()),
+        ("rotation_speed", 0.0.into()),
+        ("sequence_number", 0.into()),
+        ("sequence_number1", 0.into()),
+        ("Sort particles", true.into()),
+        ("time to sleep when not drawn", 8.0.into()),
+        ("view model effect", false.into()),
+    ])
+}
---
+++ tools/pcfstrip/src/patch.rs
@@ -0,0 +1,171 @@
+use byteorder::WriteBytesExt;
+use glob::glob;
+use std::{
+    fs::{self, File, OpenOptions},
+    io::{self, Read, Seek, SeekFrom},
+    path::Path,
+};
+
+use relative_path::RelativePathBuf;
+use thiserror::Error;
+pub use vpk::VPK;
+
+#[derive(Debug, Error)]
+pub enum PatchError {
+    #[error("file '{0}' not found in vpk")]
+    NotFound(String),
+
+    #[error("can't patch file that has preload data")]
+    HasPreloadData,
+
+    #[error("the input file size ({0} bytes) is larger than the file in the vpk archive ('{1}': {2} bytes)")]
+    InputTooBig(u64, String, u64),
+
+    #[error("only wrote {0} of the expected {1} bytes")]
+    PartialWrite(u64, u64),
+
+    #[error(transparent)]
+    IoError(#[from] io::Error),
+}
+
+pub trait PrintVpkExt {
+    fn print_all_entries(&self);
+}
+
+pub trait PatchVpkExt {
+    /// Patches data over an existing entry in the vpk's tree.
+    ///
+    /// The file on disk must have the same size as the file in the VPK, and the file must not have any preload data.
+    ///
+    /// ## Errors
+    ///
+    /// Returns [`Err`] if:
+    ///
+    /// - the file described by `path_in_vpk` does not exist in the vpk
+    /// - the file in VPK has a preload data block.
+    /// - the file on disk and file in VPK have different sizes
+    /// - the function produced no IO error but wasn't able to write the entire file
+    /// - there was an IO error when reading the file on disk
+    /// - there was an IO error when writing the file on disk
+    fn patch_file(&mut self, path_in_vpk: &str, size: u64, reader: &mut impl Read) -> Result<(), PatchError>;
+
+    /// Searches `backup_dir` PCF files recusively under the `particles` subfolder, and patches them into `self` over
+    /// files in the VPK with the same paths relative to `backup_dir`.
+    ///
+    /// ## Errors
+    ///
+    /// Returns [`Err`] if:
+    ///
+    /// - There was an error when searching the `backup_dir`
+    /// - There was an error forming a string path for a PCF
+    fn restore_particles(&mut self, backup_dir: impl AsRef<Path>) -> anyhow::Result<()>;
+}
+
+impl PrintVpkExt for vpk::VPK {
+    fn print_all_entries(&self) {
+        println!("root_path: {}", self.root_path.display());
+
+        println!("header_length: {}", self.header_length);
+        println!("version: {}", self.header.version);
+        println!("tree_length: {}", self.header.tree_length);
+        println!("signature: {}", self.header.signature);
+
+        if let Some(header_v2) = &self.header_v2 {
+            println!("chunk_hashes_length: {}", header_v2.chunk_hashes_length);
+            println!("embed_chunk_length: {}", header_v2.embed_chunk_length);
+            println!("self_hashes_length: {}", header_v2.self_hashes_length);
+            println!("signature_length: {}", header_v2.signature_length);
+        }
+
+        println!("{} entries", self.tree.len());
+
+        let mut entries: Vec<_> = self.tree.iter().clone().collect();
+        entries.sort_by(|a, b| a.1.dir_entry.archive_index.cmp(&b.1.dir_entry.archive_index));
+
+        for (key, entry) in entries {
+            if entry.dir_entry.archive_index == 0 {
+                println!("entry in {} at '{key}'", entry.dir_entry.archive_index);
+            }
+            // if let Some(archive_dir) = &entry.archive_path {
+            //     println!("- archive_dir: '{}'", archive_dir.display());
+            // }
+            // println!("- preload data len: {}", entry.preload_data.len());
+        }
+    }
+}
+
+impl PatchVpkExt for vpk::VPK {
+    fn patch_file(&mut self, path_in_vpk: &str, size: u64, reader: &mut impl Read) -> Result<(), PatchError> {
+        let entry = self
+            .tree
+            .get(path_in_vpk)
+            .ok_or_else(|| PatchError::NotFound(path_in_vpk.to_string()))?;
+
+        if entry.dir_entry.preload_length > 0 {
+            return Err(PatchError::HasPreloadData);
+        }
+
+        let Some(archive_path) = &entry.archive_path else {
+            return Err(PatchError::HasPreloadData);
+        };
+
+        // TODO: what about preload_length? does patch_file need to ever handle preloaded files?
+        let entry_size = u64::from(entry.dir_entry.file_length);
+
+        if size > entry_size {
+            return Err(PatchError::InputTooBig(size, path_in_vpk.to_string(), entry_size));
+        }
+
+        let mut archive_file = OpenOptions::new().write(true).open(archive_path.as_ref())?;
+        archive_file.seek(SeekFrom::Start(u64::from(entry.dir_entry.archive_offset)))?;
+
+        let copied = io::copy(reader, &mut archive_file)?;
+        if copied != size {
+            return Err(PatchError::PartialWrite(copied, entry_size));
+        }
+
+        // patched content needs to have the same size as the original, so we pad in 0s to make it fit snuggly.
+        if entry_size < copied {
+            for _ in entry_size..copied {
+                archive_file.write_u8(0)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn restore_particles(&mut self, backup_dir: impl AsRef<Path>) -> anyhow::Result<()> {
+        let backup_dir = backup_dir.as_ref();
+        let particles_glob = backup_dir.to_str().expect("this should never happen").to_string() + "/particles/**/*.pcf";
+        let backup_particle_paths = glob(&particles_glob)?
+            .map(|path| -> anyhow::Result<RelativePathBuf> {
+                let mut path: &Path = &path?;
+                if path.is_absolute() {
+                    path = path.strip_prefix(backup_dir)?;
+                }
+
+                Ok(RelativePathBuf::from_path(path)?)
+            })
+            .collect::<anyhow::Result<Vec<RelativePathBuf>>>()?;
+
+        // restore the particles in the misc vpk with our backup, to ensure we're at a clean state
+        for particle_file in backup_particle_paths {
+            // given ./particles/example.pcf, we should map to:
+
+            //   /particles/example.pcf - the path to the file in the VPK
+            let path_in_vpk = particle_file.clone().into_string();
+
+            //   /path/to/backup/particles/example.pcf - the actual on-disk path of the backup particle file
+            let path_on_disk = particle_file.to_path(backup_dir);
+
+            let size = fs::metadata(&path_on_disk)?.len();
+            let mut reader = File::open(&path_on_disk)?;
+
+            if let Err(err) = self.patch_file(&path_in_vpk, size, &mut reader) {
+                eprintln!("Error patching particle file '{particle_file}': {err}");
+            }
+        }
+
+        Ok(())
+    }
+}
