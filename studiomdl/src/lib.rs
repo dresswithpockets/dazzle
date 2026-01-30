69be258b581da53ccbbbed16051290e46d9b34a2
dresswithpockets <dresswithpockets@pm.me>
2026-01-30 15:06:45 -0700

    scaffold studiomdl

---
+++ studiomdl/src/lib.rs
@@ -0,0 +1,6 @@
+//! This crate is a collection of utilities for interop with studiomdl - Valve's proprietary 3D model compiler.
+//!
+//! ## Wine Dependency on Linux!
+//!
+//! Unfourtunately there are no known distributions of studiomdl which target linux natively. In order to
+//! operate studiomdl on linux, wine must be installed.
